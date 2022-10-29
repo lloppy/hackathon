1c9ece4a39b2f50d70027a4007fc70afae4fbb02
Полина <ankudinovapol@gmail.com>
2022-10-30 02:51:37 +0500

    addAct

--- app/src/main/java/com/example/haka/ItemActivity.kt
+++ app/src/main/java/com/example/haka/ItemActivity.kt
@@ -1,11 +1,18 @@
 package com.example.haka
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class ItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
+
+
     }
 }
--- app/src/main/java/com/example/haka/SubCategoryActivity.kt
+++ app/src/main/java/com/example/haka/SubCategoryActivity.kt
@@ -1,11 +1,16 @@
 package com.example.haka
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SubCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub_category)
+
+
     }
 }
