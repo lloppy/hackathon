d7189dc71b845453f361b68eb4feed221c11e2c3
Полина <ankudinovapol@gmail.com>
2022-10-29 18:19:39 +0500

    add photo

--- app/src/main/java/com/example/haka/AddActivity.kt
+++ app/src/main/java/com/example/haka/AddActivity.kt
@@ -1,11 +1,37 @@
 package com.example.haka
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class AddActivity : AppCompatActivity() {
+    val REQUEST_CODE = 100
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+        val imageView = findViewById<ImageView>(R.id.loadImageView)
+        imageView.setOnClickListener{
+            openGalleryForImage()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.loadImageView)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            imageView.setImageURI(data?.data) // handle chosen image
+        }
+    }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
     }
 }
