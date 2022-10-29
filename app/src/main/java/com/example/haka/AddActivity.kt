ec80d97bd03c10b5c719c3451104440db57826c9
Полина <ankudinovapol@gmail.com>
2022-10-29 18:45:44 +0500

    add photo to database

--- app/src/main/java/com/example/haka/AddActivity.kt
+++ app/src/main/java/com/example/haka/AddActivity.kt
@@ -2,22 +2,47 @@ package com.example.haka
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class AddActivity : AppCompatActivity() {
     val REQUEST_CODE = 100
+    lateinit var auth: FirebaseAuth
+    lateinit var name: String
 
+    lateinit var database: FirebaseDatabase
+    lateinit var myRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val nameInput = findViewById<EditText>(R.id.name)
+
         val imageView = findViewById<ImageView>(R.id.loadImageView)
         imageView.setOnClickListener{
             openGalleryForImage()
         }
 
+        auth = Firebase.auth
+        name = auth.currentUser!!.displayName.toString()
+
+        val safeButton = findViewById<Button>(R.id.saveButton)
+        safeButton.setOnClickListener {
+            writeDataToFirebase(nameInput)
+            Toast.makeText(this, "Safe!", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -33,5 +58,13 @@ class AddActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    private fun writeDataToFirebase(nameInput: EditText, ) {
+        database = Firebase.database
+        myRef = database.getReference(name)
+        myRef.setValue(nameInput.text.toString())
+
+        Toast.makeText(this, "Сохранено", Toast.LENGTH_LONG).show()
     }
 }
